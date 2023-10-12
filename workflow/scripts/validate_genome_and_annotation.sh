c1044b0fba20862d9fb393057b592252d70da0ad
Francesco Tabaro <francesco.tabaro@embl.it>
2023-10-12 16:48:38 +0200

    fix validate_genome_and_annotation.sh

--- workflow/scripts/validate_genome_and_annotation.sh
+++ workflow/scripts/validate_genome_and_annotation.sh
@@ -1,19 +1,46 @@
 #!/usr/bin/env bash
 
-set +e
+# Disable strict mode
+set +euo pipefail
 
-head -n1000 "${snakemake_input[genome_fasta_file]}" | grep -q '^>chr'
-FASTA_HAS_CHR=$?
+# Enable debug printing to stdout
+set -x
 
-head -n1000 "${snakemake_input[genome_annotation_file]}" | grep -v '#' | head -n1 | grep -q '^chr'
-GTF_HAS_CHR=$?
+# Define input and log variables
+fasta_file="${snakemake_input[genome_fasta_file]}"
+gtf_file="${snakemake_input[genome_annotation_file]}"
+log_file="${snakemake_log}"
 
-if [ $FASTA_HAS_CHR -eq 0 ] & [ $GTF_HAS_CHR -eq 1 ]; then
-    sed -i -r 's/^([^#]+)$/chr\1/g' "${snakemake_input[genome_annotation_file]}"
+# Check if input files exist
+if [ ! -f "$fasta_file" ] || [ ! -f "$gtf_file" ]; then
+    echo "Error: Input files do not exist or are inaccessible." >&2
+    exit 1
 fi
 
-if [ $FASTA_HAS_CHR -eq 1 ] & [ $GTF_HAS_CHR -eq 0 ]; then
-    sed -i -r 's/^>(.+)$/>chr\1/g' "${snakemake_input[genome_fasta_file]}"
+# Check fasta
+if head -n1000 "$fasta_file" | grep -q '^>chr'; then
+    FASTA_HAS_CHR=0
+else
+    FASTA_HAS_CHR=1
+fi
+echo "FASTA_HAS_CHR=$FASTA_HAS_CHR" > "$log_file"
+
+# Check gtf
+if head -n1000 "$gtf_file" | grep -v '#' | head -n1 | grep -q '^chr'; then
+    GTF_HAS_CHR=0
+else
+    GTF_HAS_CHR=1
+fi
+echo "GTF_HAS_CHR=$GTF_HAS_CHR" >> "$log_file"
+
+# If fasta has chr but gtf has not, add chr to gtf
+if [ "$FASTA_HAS_CHR" -eq 0 ] && [ "$GTF_HAS_CHR" -eq 1 ]; then
+    echo "Fixing GTF" >> "$log_file"
+    sed -i -r 's/^([^#]+)$/chr\1/g' "$gtf_file"
 fi
 
-
+# If gtf has chr but fasta has not, add chr to fasta
+if [ "$FASTA_HAS_CHR" -eq 1 ] && [ "$GTF_HAS_CHR" -eq 0 ]; then
+    echo "Fixing fasta" >> "$log_file"
+    sed -i -r 's/^>(.+)$/>chr\1/g' "$fasta_file"
+fi
