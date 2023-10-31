f3454e0f8a0677b70881bbabeb8a7e372da23699
Francesco Tabaro <francesco.tabaro@embl.it>
2023-10-31 15:29:07 +0100

    refine report

--- workflow/scripts/yte.py
+++ workflow/scripts/yte.py
@@ -10,35 +10,46 @@ def get_name(x: str) -> str:
     
 
 datasets = []
+views = []
+max_rows = 0
 for dataset_path in snakemake.input["datasets"]:
-    dataset_name = get_name(dataset_path)
+    dataset_name = get_name(str(dataset_path))
     dataset = {
         "name": dataset_name,
         "path": dataset_path
     }
     datasets.append(dataset)
 
-specs = [
-    (spec, json.load(open(spec, "r")))
-    for spec in snakemake.params["view_specs"]
-]
+    view = {
+        "name": dataset_name,
+        "dataset": dataset_name,
+        "type": "table"
+    }
+    views.append(view)
 
-views = []
-for (dataset_path, view_spec) in itertools.product(snakemake.input["datasets"], specs):
-    dataset_name = get_name(dataset_path)
-    spec_name = get_name(view_spec[0])
+    with open(dataset_path) as fh:
+        nlines = len(fh.readlines())
+        if nlines > max_rows:
+            max_rows = nlines
+    
+for (dataset_path, view_spec) in itertools.product(snakemake.input["datasets"], snakemake.params["view_specs"]):
+    dataset_name = get_name(str(dataset_path))
+    spec_name = get_name(str(view_spec))
     view_name = "-".join([dataset_name, spec_name])
     view = {
         "name": view_name,
         "dataset": dataset_name,
-        "spec": view_spec[1]
+        "spec_path": view_spec,
+        "type": "plot"
     }
     views.append(view)
 
 variables = {
     "plot_name": snakemake.params["plot_name"],
+    "default_view": views[0]['name'],
     "datasets": datasets,
-    "views": views
+    "views": views,
+    "max_in_memory_rows": max_rows + 1
 }
 
 with open(str(snakemake.input["template"]), "r") as template, open(
